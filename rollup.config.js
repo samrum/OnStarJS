3c7fe4684bc6ad9bb2ec11a1bbcb7d0f383e6615
Ruben Medina <dev@rubenmedina.com>
2021-01-31 17:08:15 -0800

    chore: add json import support (#32)

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,11 @@
 import typescript from "rollup-plugin-typescript2";
+import json from "@rollup/plugin-json";
 import pkg from "./package.json";
 
 export default {
   input: "src/index.ts",
   plugins: [
+    json(),
     typescript({
       typescript: require("typescript"),
     }),
