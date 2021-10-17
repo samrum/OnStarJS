b1fcd4725bebb9105b9495a3dd1822a9c9dc2aae
Ruben Medina <dev@rubenmedina.com>
2021-10-16 18:45:10 -0700

    chore: use @rollup/plugin-typescript for builds

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,10 @@
-import typescript from "rollup-plugin-typescript2";
 import json from "@rollup/plugin-json";
+import typescript from "@rollup/plugin-typescript";
 import pkg from "./package.json";
 
 export default {
   input: "src/index.ts",
-  plugins: [
-    json(),
-    typescript({
-      typescript: require("typescript"),
-    }),
-  ],
+  plugins: [json(), typescript({ tsconfig: "./tsconfig.json" })],
   output: [
     { file: pkg.main, format: "cjs", exports: "default" },
     { file: pkg.module, format: "esm", exports: "default" },
